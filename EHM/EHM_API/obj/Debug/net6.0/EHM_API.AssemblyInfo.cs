0f054b9518d1192d97600ce99bc3f72c796e469e
nhantacb <you@example.com>
2024-06-11 14:48:00 +0700

    add addressid to order

--- EHM/EHM_API/obj/Debug/net6.0/EHM_API.AssemblyInfo.cs
+++ EHM/EHM_API/obj/Debug/net6.0/EHM_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EHM_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa52ce49a5aee8896fa55981f945be3f737a9c2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eb76275f50ac0312074d19f24cc09c8ef2172da")]
 [assembly: System.Reflection.AssemblyProductAttribute("EHM_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EHM_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
