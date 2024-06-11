15953e68cb2985cdb9341e8b49953533aacf8cf3
HanhTaDuy <106169560+HanhTaDuy@users.noreply.github.com>
2024-06-11 16:16:19 +0700

    update fill dish

---
+++ EHM/EHM_API/Services/ITableService.cs
@@ -0,0 +1,14 @@
+﻿using EHM_API.Models;
+
+namespace EHM_API.Services
+{
+    public interface ITableService
+    {
+        Task<IEnumerable<Table>> GetAllAsync();
+        Task<Table> GetByIdAsync(int id);
+        Task<Table> CreateAsync(Table table);
+        Task<Table> UpdateAsync(Table table);
+        Task<bool> ChangeStatusAsync(int id, string status);
+        Task<IEnumerable<Table>> SearchAsync(string keyword);
+    }
+}
