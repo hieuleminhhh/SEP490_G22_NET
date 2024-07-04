253f0f2fabcb94f1ecae63bc9ce32250b87c11bb
nhantacb <nhantacbvn@gmail.com>
2024-07-04 17:02:21 +0700

    update search seservation2

--- EHM/EHM_API/obj/Debug/net6.0/EHM_API.AssemblyInfo.cs
+++ EHM/EHM_API/obj/Debug/net6.0/EHM_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EHM_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fc6ccac98b41cdb0ace2c29b9d0def64ae50e3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a2726808e8c536cd953e05e5e411ad198c05015")]
 [assembly: System.Reflection.AssemblyProductAttribute("EHM_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EHM_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
