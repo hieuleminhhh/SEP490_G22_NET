4f81216544361f3c714483f9cb7aabb6c0b74d52
DuongLV <ld4902@gmail.com>
2024-07-21 15:59:48 +0700

    update vnpay

--- EHM/EHM_API/DTOs/VnPayDTO/VnPaymentResponseModel.cs
+++ EHM/EHM_API/DTOs/VnPayDTO/VnPaymentResponseModel.cs
@@ -14,10 +14,10 @@
 
     public class VnPaymentRequestModel
     {
-        public int OrderId { get; set; }
-        public string FullName { get; set; }
-        public string Description { get; set; }
-        public double Amount { get; set; }
-        public DateTime CreatedDate { get; set; }
+        public int? OrderId { get; set; }
+        public string? FullName { get; set; }
+        public string? Description { get; set; }
+        public decimal? Amount { get; set; }
+        public DateTime? CreatedDate { get; set; }
     }
 }
