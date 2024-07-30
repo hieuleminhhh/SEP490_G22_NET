932f3edf7d24e7cb6522623eb813e7d2d15b6c71
hieuleminhhh <hieuleminhhhhh@gmail.com>
2024-07-30 17:10:16 +0700

    update odd

--- EHM/EHM_API/DTOs/OrderDTO/Manager/GetOrderDetailDTO.cs
+++ EHM/EHM_API/DTOs/OrderDTO/Manager/GetOrderDetailDTO.cs
@@ -2,17 +2,17 @@
 {
 	public class GetOrderDetailDTO
 	{
-		public int OrderDetailId { get; set; }
-		public int? DishId { get; set; }
-		public string? ItemName { get; set; }
 		public int? ComboId { get; set; }
-		public string? NameCombo { get; set; }
-		public string? ImageUrl { get; set; }
+        public decimal? DiscountedPrice { get; set; }
+		public int? DishId { get; set; }
 		public int? DishesServed { get; set; }
-		public decimal? UnitPrice { get; set; }
-		public decimal? DiscountedPrice { get; set; }
-		public int? Quantity { get; set; }
-	
+        public string? ImageUrl { get; set; }
+        public string? ItemName { get; set; }
+		public string? NameCombo { get; set; }
+
+		public decimal? Price { get; set; }
+        public int? Quantity { get; set; }
+        public decimal? UnitPrice { get; set; }	
 		public string? Note { get; set; }
 		public DateTime? OrderTime { get; set; }
 	}
