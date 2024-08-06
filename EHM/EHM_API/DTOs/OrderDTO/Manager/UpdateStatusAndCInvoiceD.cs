2cf5c14ba6f03a97908401225e7cbe9fe4ca0e3f
nhantacb <nhantacbvn@gmail.com>
2024-08-06 22:08:07 +0700

    Create api Update status and create invoice

---
+++ EHM/EHM_API/DTOs/OrderDTO/Manager/UpdateStatusAndCInvoiceD.cs
@@ -0,0 +1,17 @@
+﻿namespace EHM_API.DTOs.OrderDTO.Manager
+{
+	public class UpdateStatusAndCInvoiceD
+	{
+		public int Status { get; set; }
+
+		public DateTime? PaymentTime { get; set; }
+		public decimal? PaymentAmount { get; set; }
+		public string? Taxcode { get; set; }
+		public int PaymentStatus { get; set; }
+		public int? AccountId { get; set; }
+		public decimal? AmountReceived { get; set; }
+		public decimal? ReturnAmount { get; set; }
+		public int? PaymentMethods { get; set; }
+		public string? Description { get; set; }
+	}
+}
