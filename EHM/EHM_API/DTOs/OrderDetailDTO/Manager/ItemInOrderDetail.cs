4fa0bd9f99e9bf555502a7dca497a262572c31d0
HanhTaDuy <106169560+HanhTaDuy@users.noreply.github.com>
2024-08-24 05:03:45 +0700

    update orderdetailStaff

--- EHM/EHM_API/DTOs/OrderDetailDTO/Manager/ItemInOrderDetail.cs
+++ EHM/EHM_API/DTOs/OrderDetailDTO/Manager/ItemInOrderDetail.cs
@@ -4,7 +4,9 @@
     {
         public int OrderDetailId { get; set; }
         public string? ItemName { get; set; }
+        public decimal? ItemPrice { get; set; }
         public string? ComboName { get; set; }
+        public decimal? ComboPrice { get; set; }
         public int? Quantity { get; set; }
         public decimal? UnitPrice { get; set; }
         public int? DishesServed { get; set; }
