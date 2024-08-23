6585e59d69bc05fee04506edbe52ac2021b73a52
HanhTaDuy <106169560+HanhTaDuy@users.noreply.github.com>
2024-08-24 04:52:00 +0700

    update orderforstaff

--- EHM/EHM_API/Map/MapperConfig.cs
+++ EHM/EHM_API/Map/MapperConfig.cs
@@ -660,8 +660,8 @@ namespace EHM_API.Map
                 .ForMember(dest => dest.ComboName, opt => opt.MapFrom(src => src.Combo != null ? src.Combo.NameCombo : null))
                 .ForMember(dest => dest.Quantity, opt => opt.MapFrom(src => src.Quantity))
                 .ForMember(dest => dest.DishesServed, opt => opt.MapFrom(src => src.DishesServed))
-                .ForMember(dest => dest.OrderTime, opt => opt.MapFrom(src => src.OrderTime))
-                .ForMember(dest => dest.ItemTotalPrice, opt => opt.MapFrom(src => (src.UnitPrice ?? 0) * (src.Quantity ?? 0)));
+                .ForMember(dest => dest.OrderTime, opt => opt.MapFrom(src => src.OrderTime));
+               
 
             //Account
             CreateMap<Account, GetAccountDTO>();
