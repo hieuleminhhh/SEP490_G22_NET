9cb8dedba54de8b734fa8743bd352b3dcb153069
nhantacb <nhantacbvn@gmail.com>
2024-08-27 21:23:25 +0700

    Upfs

--- EHM/EHM_API/DTOs/OrderDTO/Manager/OrderDTO.cs
+++ EHM/EHM_API/DTOs/OrderDTO/Manager/OrderDTO.cs
@@ -15,7 +15,8 @@ namespace EHM_API.DTOs.OrderDTO.Manager
         public string? GuestPhone { get; set; }
         public decimal Deposits { get; set; }
         public int AddressId { get; set; }
-        public string? GuestAddress { get; set; }
+		public int? DiscountPercent { get; set; }
+		public string? GuestAddress { get; set; }
         public string? ConsigneeName { get; set; }
         public string? Note { get; set; }
 		public int? Type { get; set; }
