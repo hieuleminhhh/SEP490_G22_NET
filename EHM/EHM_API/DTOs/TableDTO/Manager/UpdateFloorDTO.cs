408914d8697826406de3c8ba4c6b9bad8aa94d0a
HanhTaDuy <106169560+HanhTaDuy@users.noreply.github.com>
2024-09-11 17:59:29 +0700

    update table, update order by account

---
+++ EHM/EHM_API/DTOs/TableDTO/Manager/UpdateFloorDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace EHM_API.DTOs.TableDTO.Manager
+{
+    public class UpdateFloorDTO
+    {
+        public int CurrentFloor { get; set; }  // Tầng hiện tại
+        public int NewFloor { get; set; }
+       
+    }
+}
